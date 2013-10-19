19f92f3a61f92f1aa1bff64f47e0db83a72ba840
Paul George <p.a.george19@gmail.com>
2013-10-18 22:33:39 -0400

    calculations on models

--- js/munter/munter2.js
+++ js/munter/munter2.js
@@ -1,6 +1,6 @@
 mA = angular.module("msApp2", []);
 
-mA.controller("RepeatModelCtrl", ["$scope",
+mA.controller("MainCtrl", ["$scope",
     function($scope) {
         $scope.models = [];
 
@@ -8,11 +8,7 @@ mA.controller("RepeatModelCtrl", ["$scope",
         $scope.addModel = function() {
             // should use AngularJS Service here to create a new model
             // ...but that's for later...
-            $scope.models.push({
-                // name: "",
-                // prop1: "",
-                // prop2: ""
-            });
+            $scope.models.push({});
         };
 
         $scope.save = function(model){
@@ -25,5 +21,29 @@ mA.controller("RepeatModelCtrl", ["$scope",
             // JS method for removing an element from an Array is "pop(...)"
             $scope.models.pop(model);
         };
+
+        $scope.propTotal = function() {
+            var s = 0;
+            for(var i = 0; i < $scope.models.length; i++) {
+                // check if is numeric...
+                v = parseInt($scope.models[i].prop, 10);
+                if (isNaN(v)) {
+                    return "NaN";
+                } else {
+                    s += v;
+                }
+            }
+            return s;
+        };
+
+        // NOTE: this gets called everytime the value of "propTotal()" changes.
+        $scope.$watch("propTotal()", function() {
+            // alias
+            models = $scope.models;
+            $scope.diffs = [];
+            for(var i = 0; i < models.length; i++) {
+                $scope.diffs.push(i > 0 ? (models[i].prop - models[i-1].prop) : "n/a");
+            }
+        });
     }
 ]);
